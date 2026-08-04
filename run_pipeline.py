1606cda3aba8cbfc737f3a77b86fcdacc7ea48b9
Nimisha Iyer <155137074+student-nimisha@users.noreply.github.com>
2026-08-04 16:15:29 +0530

    Update run_pipeline.py

--- run_pipeline.py
+++ run_pipeline.py
@@ -27,7 +27,7 @@ CONFIG = {
     "mode": "demo",
 
     # only used when mode == "demo"
-    "demo_text": "nee oru mosham manushyan aan",
+    "demo_text": "you are stupid",
 
     # ---- MODEL ----
     # Swap models here. Examples that already work:
